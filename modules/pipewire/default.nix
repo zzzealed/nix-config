c9a3174f45b1bd0271eea4dcf8dc7883cf37f530
Zeal <zzzealed@proton.me>
2025-10-13 13:31:20 +0200

    switch to plain Pipewire

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -3,9 +3,9 @@
   imports = [ inputs.nix-gaming.nixosModules.pipewireLowLatency ];
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
+    #pulse.enable = true;
 
     lowLatency = {
       # enable this module
