635ca4952b4fbd2845a6cf63554577143e22ced6
Zeal <zzzealed@proton.me>
2025-10-18 12:37:07 +0200

    stylix fonts and specilisation

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -3,23 +3,35 @@
   stylix = {
     enable = true;
     autoEnable = true;
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing-light.yaml";
     image = ./config/wallhaven-rr5rq7_1920x1080.png;
-#    fonts = { # TODO
-#      emoji = {
-#        name = "";
-#        package = "";
-#      };
-#    };
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.code-new-roman;
+        name = "";
+      };
+    };
     cursor = {
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
       size = 24;
     };
     icons.enable = true;
-    #polarity = "dark";
-    polarity = "light";
     opacity.popups = "0.75";
+ };
+ # A specialisation you can use when rebuild with `--specialistation <light-theme | dark-theme>`
+ # See: https://wiki.nixos.org/wiki/Specialisation
+ specialisation = {
+   light-theme.configuration = {
+     stylix = {
+       base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing-light.yaml";
+       polarity = "light";
+     };
+   };
+   dark-theme.configuration = {
+     stylix = {
+       base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
+       polarity = "dark";
+     };
+   };
  };
 }
