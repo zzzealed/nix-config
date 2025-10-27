d95de86995b1e0a878bf0c3e9021c71d41c1b195
Zeal <zzzealed@proton.me>
2025-10-27 09:20:11 +0100

    import flakeinputs per module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,10 @@
-{ flakeInputs, flakeOutputs, pkgs, ... }:
+{ inputs, flakeInputs, flakeOutputs, pkgs, ... }:
 {
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
   environment.systemPackages = [ pkgs.home-manager ];
   home-manager = {
     #useGlobalPkgs = true; # Apparently deprecated?
-    extraSpecialArgs = { inherit flakeInputs flakeOutputs; };
+    extraSpecialArgs = { inherit flakeInputs flakeOutputs inputs; };
     backupFileExtension = "bak";
     users.mads.home.stateVersion = "24.11";
   };
