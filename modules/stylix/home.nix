9935c497b22cef05200b1c5d6ccf3cf531d45100
Zeal <zzzealed@proton.me>
2025-11-05 10:58:42 +0100

    refactor Stylix

--- modules/stylix/home.nix
+++ modules/stylix/home.nix
@@ -1,9 +1,18 @@
 { ... }:
 {
-  stylix.targets = {
-    swaylock = {
-      enable = true;
-      useWallpaper = true;
+  # TODO: This is a weird place to put these but I don't know where else to put them
+  services.wpaperd.enable = true;
+  services.swaync.enable = true;
+  programs.swaylock.enable = true;
+  programs.fuzzel.enable = true;
+  programs.mpv.enable = true;
+  programs.bat.enable = true;
+
+  stylix = {
+    targets = {
+      swaylock = {
+        useWallpaper = true;
+      };
     };
   };
 }
