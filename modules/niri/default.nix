3498da17f0a1f06a16df51195dc6a2e0fcb5a118
Zeal <zzzealed@proton.me>
2025-11-06 13:49:24 +0100

    add libnotify package

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -8,6 +8,7 @@
     unstable.xwayland-satellite # X-compatibility
     fuzzel # TODO: Wofi?
     swaynotificationcenter # Notification
+    libnotify # swaync dep
     swaylock # Lockscreen, TODO: Get another one
     wpaperd # Wallpaper
     wl-clipboard-rs # Clipboard, I am NOT beating the allegations
