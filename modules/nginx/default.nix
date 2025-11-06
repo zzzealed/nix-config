a0445b9337cd870a527fdd7315cadb951d1bda05
Zeal <zzzealed@proton.me>
2025-11-06 11:31:33 +0100

    add Porkbun credentials

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -1,8 +1,39 @@
-{ ... }:
+{ config, ... }:
 {
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  age.secrets."porkbun-nginx_api_key".file = ../../secrets/porkbun-nginx_api_key.age;
+  age.secrets."porkbun-nginx_api_secret".file = ../../secrets/porkbun-nginx_api_secret.age;
   services.nginx = {
     enable = true;
-    recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
     recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = { # This is just an example. I usually set this per-service in their respective module.
+      "zzzealed.com" = {
+        useACMEHost = "zzzealed.com";
+        forceSSL = true;
+        root = "/";
+      };
+    };
   };
+  # TODO: TLS reverse-proxy?
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "zzzealed@proton.me";
+    certs."zzzealed.com" = {
+      domain = "zzzealed.com";
+      extraDomainNames = [ 
+        "*.zzzealed.com"
+        "*.l.zzzealed.com"
+      ];
+      dnsProvider = "porkbun";
+      dnsPropagationCheck = true;
+      credentialFiles = {
+        PORKBUN_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_key".path;
+        PORKBUN_SECRET_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_secret".path;
+      };
+    };
+  };
+  users.users.nginx.extraGroups = [ "acme" ];
 }
