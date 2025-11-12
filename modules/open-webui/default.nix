929a58cd6e15cbe831822ecae0468f82da0d328a
Zeal <zzzealed@proton.me>
2025-11-12 11:41:06 +0100

    add g4f provider endpoints

--- modules/open-webui/default.nix
+++ modules/open-webui/default.nix
@@ -9,14 +9,15 @@
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
       SCARF_NO_ANALYTICS = "True";
-      OPENAI_API_BASE_URL = "http://server.l.zzzealed.com:9090/api/Perplexity"; # GPT4Free instance, shhh
+      #OPENAI_API_BASE_URL = "http://server.l.zzzealed.com:9090/v1"; # GPT4Free instance, shhh
+      OPENAI_API_BASE_URLS = "http://server.l.zzzealed.com:9090/api/Perplexity;http://server.l.zzzealed.com:9090/api/PollinationsAI";
       ENABLE_SIGNUP = "False";
-      DEFAULT_MODELS = "claude45sonnetthinking";
+      DEFAULT_MODELS = "claude45sonnet";
       ENABLE_PERSISTENT_CONFIG = "True"; # We need this I guess?
       ENABLE_VERSION_UPDATE_CHECK = "False";
       ENABLE_WEB_SEARCH = "True";
       WEB_SEARCH_ENGINE = "searxng";
-      SEARXNG_QUERY_URL = "https://searx.l.zzzealed.com/search?q=<query>"; # Remember to enable JSON on SearX instance
+      SEARXNG_QUERY_URL = "https://searx.l.zzzealed.com/search?q=<query>"; # Remember to enable JSON on SearXNG instance
     };
   };
   services.nginx = {
