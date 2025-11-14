bdaaaa7213bb444c956c37417e067af3bc89a770
Zeal <zzzealed@proton.me>
2025-11-14 22:12:04 +0100

    update 1.21.8 to 1.21.10

--- modules/minecraft-server/default.nix
+++ modules/minecraft-server/default.nix
@@ -9,11 +9,11 @@
     openFirewall = true;
     servers.fabric = {
       enable = true;
-      package = pkgs.fabricServers.fabric-1_21_8.override { loaderVersion = "0.17.3"; };
+      package = pkgs.fabricServers.fabric-1_21_10.override { loaderVersion = "0.17.3"; };
     };
     servers.mads-sp = {
       enable = true;
-      package = pkgs.fabricServers.fabric-1_21_8;
+      package = pkgs.fabricServers.fabric-1_21_10;
       openFirewall = true;
       whitelist = {
         # This is a mapping from Minecraft usernames to UUIDs. You can use https://mcuuid.net/ to get a Minecraft UUID for a username
@@ -45,14 +45,24 @@
       jvmOpts = "-Xms2048M -Xmx12288M"; # Inital 2gb, then max 12gb
         symlinks = {
           mods = pkgs.linkFarmFromDrvs "mods" ( builtins.attrValues {
-            Fabric-API = pkgs.fetchurl          { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/RMahJx2I/fabric-api-0.136.0%2B1.21.8.jar"; sha256 = "sha256-EDZwSZGz77+KPQP/04NVJ0ywAgKuRcdoqPYwwZTRLtY="; };
-            FerriteCore = pkgs.fetchurl         { url = "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar"; sha256 = "sha256-K5C/AMKlgIw8U5cSpVaRGR+HFtW/pu76ujXpxMWijuo="; };
-            Lithium = pkgs.fetchurl             { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/qxIL7Kb8/lithium-fabric-0.18.1%2Bmc1.21.8.jar"; sha256 = "sha256-BPNwwvboGdzYajxWhOOP0enEmIx8JJP/srwH60UEuUw="; };
-            YetAnotherConfigLib = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/1eAoo2KR/versions/WxYlHLu6/yet_another_config_lib_v3-3.7.1%2B1.21.6-fabric.jar"; sha256 = "sha256-fXH77g4soPOKR1MJ0jydCEzWjt1L5lRO7Nta7B820e0="; };
-            SimpleVoiceChat = pkgs.fetchurl     { url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/2Z1g1v36/voicechat-fabric-1.21.8-2.6.6.jar"; sha256 = "sha256-/vvxvBq5YY6Tuu3UarBXmdiVN0hOVTwL/eNX3HLo0yE="; };
-            Krypton = pkgs.fetchurl             { url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/neW85eWt/krypton-0.2.9.jar"; sha256 = "sha256-uGYia+H2DPawZQxBuxk77PMKfsN8GEUZo3F1zZ3MY6o="; };
-            C2M = pkgs.fetchurl                 { url = "https://cdn.modrinth.com/data/VSNURh3q/versions/tlZRTK1v/c2me-fabric-mc1.21.8-0.3.4.0.0.jar"; sha256 = "sha256-MkYZOI9AL7yswqEfSp8/Njs4z7xFRItuvLu96znO/v4="; };
-            ImprovedSigns = pkgs.fetchurl       { url = "https://cdn.modrinth.com/data/tEcCNQe7/versions/Snd745di/improved-signs-1.5.1%2Bmc1.21.6.jar"; sha256 = "sha256-e3QLT+vCNT/TT/CnM3hzSbU7an2+P6xfkDVI7Va9fRo="; };
+            Fabric-API = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/dQ3p80zK/fabric-api-0.138.3%2B1.21.10.jar";
+                                         sha256 = "sha256-rCB1kEGet1BZqpn+FjliQEHB1v0Ii6Fudi5dfs9jOVM="; };
+            FerriteCore = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/uXXizFIs/versions/MGoveONm/ferritecore-8.0.2-fabric.jar";
+                                          sha256 = "sha256-LGn9gXMEu2l1zUti/TK/IaXVyPDDUj6sxzTAwlB+2nc="; };
+            Lithium = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/NsswKiwi/lithium-fabric-0.20.1%2Bmc1.21.10.jar";
+                                      sha256 = "sha256-R4Z0hlDjXdFlcrhenM0YOadgbATGdqWbZEUOhEadKJo="; };
+            YetAnotherConfigLib = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/1eAoo2KR/versions/UZmV6xf4/yet_another_config_lib_v3-3.8.0%2B1.21.9-fabric.jar";
+                                                  sha256 = "sha256-Su3K8eC2kHqCrIn2R9j4cuzBVPZzvVhE0FrsGvz29DY="; };
+            SimpleVoiceChat = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/BjR2lc4k/voicechat-fabric-1.21.10-2.6.6.jar";
+                                              sha256 = "sha256-yC5pMBLsi4BnUq4CxTfwe4MGTqoBg04ZaRrsBC3Ds3Y="; };
+            Krypton = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/O9LmWYR7/krypton-0.2.10.jar";
+                                      sha256 = "sha256-lCkdVpCgztf+fafzgP29y+A82sitQiegN4Zrp0Ve/4s="; };
+            C2MFabric = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/VSNURh3q/versions/2EKX8Hiv/c2me-fabric-mc1.21.10-0.3.6%2Balpha.0.9.jar";
+                                        sha256 = "sha256-CKyjlpFTLY3a/+zWWFieCc0aQW9sdUektPUSZhQDsFo="; };
+            #ImprovedSigns = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/tEcCNQe7/versions/qj2uONOj/improved-signs-1.6.0%2Bmc1.21.9.jar";
+            #                                sha256 = "sha256-iPYknfreQ0ZqmdL/1FCMhoLpLb3AdgW5AeqOKmI68Ns="; };
+            ScalableLux = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/Ps1zyz6x/versions/PV9KcrYQ/ScalableLux-0.1.6%2Bfabric.c25518a-all.jar";
+                                          sha256 = "sha256-ekpzcThhg8dVUjtWtVolHXWsLCP0Cvik8PijNbBdT8I="; };
           } );
         };
       };
