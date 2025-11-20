59736b740296280b813876fd9ea2e2daecf9683e
Zeal <zzzealed@proton.me>
2025-11-20 12:28:43 +0100

    newline

--- modules/ffmpeg/default.nix
+++ modules/ffmpeg/default.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [ ((pkgs.ffmpeg-full.override { withUnfree = true; }).overrideAttrs (_: { doCheck = false; })) ]; # https://wiki.nixos.org/wiki/FFmpeg#Speeding_up_install
+  environment.systemPackages = [
+    ((pkgs.ffmpeg-full.override { withUnfree = true; }).overrideAttrs (_: { doCheck = false; })) # https://wiki.nixos.org/wiki/FFmpeg#Speeding_up_install
+  ];
 }
 
