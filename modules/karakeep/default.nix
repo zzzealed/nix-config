ccc4f0bf80e5cb8c104872adc9aa107ab36792a7
Zeal <zzzealed@proton.me>
2025-11-27 10:23:35 +0100

    add inference env vars

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -1,11 +1,17 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   services.karakeep = {
     enable = true;
+    package = pkgs.unstable.karakeep;
     extraEnvironment = {
       PORT = "7890";
       DISABLE_SIGNUPS = "true";
       DISABLE_NEW_RELEASE_CHECK = "true";
+      OPENAI_API_KEY = "secret";
+      OPENAI_BASE_URL = "http://server.l.zzzealed.com:9090/v1";
+      INFERENCE_TEXT_MODEL = "deepseek-v3";
+      INFERENCE_IMAGE_MODEL = "gptimage";
+      INFERENCE_CONTEXT_LENGTH = "16384";
     };
   };
   #networking.firewall = { allowedTCPPorts = [ 7890 ]; };
