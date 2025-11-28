c6ee4a25e217e7d6d33d33676a058a2fc8e8b60f
Zeal <zzzealed@proton.me>
2025-11-28 19:53:49 +0100

    add crawler options

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -7,11 +7,17 @@
       PORT = "7890";
       DISABLE_SIGNUPS = "true";
       DISABLE_NEW_RELEASE_CHECK = "true";
+      # Inference
       OPENAI_API_KEY = "secret";
       OPENAI_BASE_URL = "http://server.l.zzzealed.com:9090/v1";
       INFERENCE_TEXT_MODEL = "deepseek-v3";
       INFERENCE_IMAGE_MODEL = "gptimage";
       INFERENCE_CONTEXT_LENGTH = "16384";
+      # Crawler
+      CRAWLER_FULL_PAGE_SCREENSHOT = "true";
+      CRAWLER_FULL_PAGE_ARCHIVE = "true";
+      CRAWLER_VIDEO_DOWNLOAD = "true";
+      CRAWLER_VIDEO_DOWNLOAD_MAX_SIZE = "-1";
     };
   };
   #networking.firewall = { allowedTCPPorts = [ 7890 ]; };
