8db79d25e7e68dd94b323b7bcf02c28a3a7ff406
Zeal <zzzealed@proton.me>
2025-12-06 02:21:30 +0100

    new graalvm-ce package name

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -1,17 +1,18 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-  (prismlauncher.override {
-    # Add binary required by some mod
-    additionalPrograms = [ ffmpeg ];
-
-    # Change Java runtimes available to Prism Launcher
-    jdks = [
-      graalvm-ce
-      zulu8
-      zulu17
-      zulu
-    ];
-  })
-];
+   environment.systemPackages = [ inputs.prismlauncher.packages.${pkgs.system}.prismlauncher ];
+#  environment.systemPackages = with pkgs; [
+#  (prismlauncher.override {
+#    # Add binary required by some mod
+#    additionalPrograms = [ ffmpeg ];
+#
+#    # Change Java runtimes available to Prism Launcher
+#    jdks = [
+#      graalvmPackages.graalvm-ce
+#      zulu8
+#      zulu17
+#      zulu
+#    ];
+#  })
+#  ];
 }
