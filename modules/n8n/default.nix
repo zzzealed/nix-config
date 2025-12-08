41f4ac87d2b9a1f6e1672273aa2c4870c3f4f043
Zeal <zzzealed@proton.me>
2025-12-08 18:04:33 +0100

    new env var names

--- modules/n8n/default.nix
+++ modules/n8n/default.nix
@@ -3,16 +3,15 @@
   services.n8n = {
     enable = true;
     #openFirewall = true;
-    webhookUrl = "";
-    settings = {
-      port = 5678;
+    environment = {
+      N8N_PORT = 5678;
     };
   };
   services.nginx = {
     virtualHosts."n8n.l.zzzealed.com" = {
       useACMEHost = "zzzealed.com";
       forceSSL = true;
-      locations."/".proxyPass = "http://127.0.0.1:${toString config.services.n8n.settings.port}";
+      locations."/".proxyPass = "http://127.0.0.1:${toString config.services.n8n.environment.N8N_PORT}";
     };
   };
 }
