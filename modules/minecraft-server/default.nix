5b0794ab678abd2e18f690375fc62f23391a76a5
Zeal <zzzealed@proton.me>
2025-12-12 10:24:31 +0100

    open 24454 port

--- modules/minecraft-server/default.nix
+++ modules/minecraft-server/default.nix
@@ -3,6 +3,8 @@
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
+  networking.firewall = { allowedUDPPorts = [ 24454 ]; }; # For "Simple Voice Chat" mod.
+
   services.minecraft-servers = {
     enable = true;
     eula = true;
