f9b8a361f6565000799bfaefb16375c0cd7e6330
Zeal <zzzealed@proton.me>
2025-12-24 16:45:08 +0100

    switch to domain

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -9,7 +9,7 @@
       DISABLE_NEW_RELEASE_CHECK = "true";
       # Inference
       OPENAI_API_KEY = "secret";
-      OPENAI_BASE_URL = "http://server.l.zzzealed.com:9090/v1";
+      OPENAI_BASE_URL = "https://g4f.l.zzzealed.com/v1";
       INFERENCE_TEXT_MODEL = "deepseek-v3";
       INFERENCE_IMAGE_MODEL = "gptimage";
       INFERENCE_CONTEXT_LENGTH = "16384";
