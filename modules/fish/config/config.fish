a2e1396da0d5bbe1ad30a15c4f22da72e2159d3d
Zeal <zzzealed@proton.me>
2025-12-26 01:45:31 +0100

    remove btm alias

--- modules/fish/config/config.fish
+++ modules/fish/config/config.fish
@@ -6,7 +6,6 @@ alias grep rg
 alias ls "ls --color=always --indicator-style=slash"
 alias mv "uutils-mv --verbose --interactive --progress"
 alias rm "rm --verbose --interactive=once"
-alias btm "btm --enable_gpu"
 alias find fd
 alias fzf sk
 alias vid_length "bash /home/mads/nix-config/modules/bash/config/list_vid_length.sh"
