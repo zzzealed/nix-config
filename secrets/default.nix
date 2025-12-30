90697819a92be1e6380452eb2331a22088770bed
Zeal <zzzealed@proton.me>
2025-12-30 21:38:03 +0100

    resign keys

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,7 +1,7 @@
-{ inputs, stdenv, ... }:
+{ inputs, system, ... }:
 {
   imports = [ inputs.agenix.nixosModules.default ];
-  environment.systemPackages = [ inputs.agenix.packages.${stdenv.hostPlatform.system}.default ];
+  environment.systemPackages = [ inputs.agenix.packages.${system}.default ];
   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # /etc/ssh/ssh_host_ed25519_key
 }
 
