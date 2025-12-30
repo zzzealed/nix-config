0952129dc1e1a0b4531519c2f50e52a363801504
Zeal <zzzealed@proton.me>
2025-12-30 21:38:03 +0100

    resign keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
+  # Inside `/etc/ssh`
   root_desktop-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEU3Cy4L/zlUm3lJmVbeN5XnnKA1us4incL+d6JH4Dg9 root@nixos";
   root_server-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/rJS1hRE1N7E7B52yBPb1fkDwWaP6oYtF+3/ArdK8l root@nixos";
-  decodingKeys = [ root_desktop-nixos root_server-nixos ];
-    
+  root_pi-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBzXNbSFzEjnQS+8D3guVv1UedC2E9EK0MZLaBDXMe+c root@nixos";
+  decodingKeys = [ root_desktop-nixos root_server-nixos root_pi-nixos ];
+  
   mads_desktop-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0bskvLTrkiFLQeS4K1uc8EwNGXrCcigrRZa/dPcycI mads@desktop-nixos";
   mads_server-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOo+0J8abiPou6UWCuolKS0RriZ5zAYrgV2zdgIYTuQ5 mads@server-nixos";
   interactiveKeys = [ mads_desktop-nixos mads_server-nixos ];
@@ -50,6 +52,10 @@ in
   };
   "glance-releases-token_github.age" = {
     publicKeys = [ root_server-nixos ] ++ interactiveKeys;
+    armor = true;
+  };
+  "wireguard_private-key.age" = {
+    publicKeys = [ root_pi-nixos ] ++ interactiveKeys;
     armor = true;
   };
 }
