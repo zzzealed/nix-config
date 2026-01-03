d09e51b8dd173a5ffabf5e59be5f102faae36f0a
Zeal <zzzealed@proton.me>
2026-01-03 01:26:45 +0100

    add cachix

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -1,8 +1,4 @@
 { inputs, pkgs, ... }:
 {
-  nix.settings = {
-     trusted-substituters = [ "https://prismlauncher.cachix.org" ];
-     trusted-public-keys = [ "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c=" ];
-  };
   environment.systemPackages = [ inputs.prismlauncher.packages.${pkgs.system}.prismlauncher ];
 }
