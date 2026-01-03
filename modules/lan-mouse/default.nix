e3e58ecb46613952052ab4bce276b299b55b4b7f
Zeal <zzzealed@proton.me>
2026-01-03 01:27:00 +0100

    add cachix

--- modules/lan-mouse/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-{
-  nix.settings = {
-    extra-substituters = [ "https://lan-mouse.cachix.org" ];
-    extra-trusted-public-keys = [ "lan-mouse.cachix.org-1:KlE2AEZUgkzNKM7BIzMQo8w9yJYqUpor1CAUNRY6OyM=" ];
-  };
-}
