d4aaf3e02aeded0bf91dc2588b851d0f32687d06
Zeal <zzzealed@proton.me>
2026-01-13 05:19:26 +0100

    add thumbnail boilerplate

--- modules/mpv/default.nix
+++ modules/mpv/default.nix
@@ -5,6 +5,14 @@
       mpvScripts.webtorrent-mpv-hook
       mpvScripts.sponsorblock-minimal
       mpvScripts.mpris
+#      (mpvScripts.thumbnail.overrideAttrs (oldAttrs: {
+#        src = pkgs.fetchFromGitHub {
+#          owner = "Alistair1231";
+#          repo = "mpv_thumbnail_script";
+#          rev = "v0.5.4+1";
+#          sha256 = "sha256-0JXnR3oWkPKTC+Io+Rkb77WU/qyoGbue1bIa2fzEvWE=";
+#        };
+#      }))
     ]; } )
   ];
 }
