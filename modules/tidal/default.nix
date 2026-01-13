0b390ad25e7b2f52dcd91a9b8bdf07d3162ad3d9
Zeal <zzzealed@proton.me>
2026-01-13 05:17:10 +0100

    tidaluna is about as fucking stable as antimatter

--- modules/tidal/default.nix
+++ modules/tidal/default.nix
@@ -2,7 +2,7 @@
 {
   # TODO: ${stdenv.hostPlatform.system}
   environment.systemPackages = [
-    #pkgs.unstable.tidal-hifi
-    inputs.tidaluna.packages.${system}.default
+    pkgs.unstable.tidal-hifi
+    #inputs.tidaluna.packages.${system}.default
   ];
 }
