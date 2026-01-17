bd02d1ba0b3d0b9efedefb24e57b0fce014e173c
Zeal <zzzealed@proton.me>
2026-01-17 16:52:31 +0100

    add radicale settings

--- modules/radicale/default.nix
+++ modules/radicale/default.nix
@@ -2,5 +2,25 @@
 {
   services.radicale = {
     enable = true;
+    settings = {
+      server = {
+        hosts = [ "0.0.0.0:5232" "[::]:5232" ];
+      };
+      auth = {
+        type = "htpasswd";
+        htpasswd_filename = "/etc/radicale/users";
+        htpasswd_encryption = "bcrypt";
+      };
+      storage = {
+        filesystem_folder = "/var/lib/radicale/collections";
+      };
+    };
+  };
+  services.nginx = {
+    virtualHosts."radicale.l.zzzealed.com" = {
+      useACMEHost = "zzzealed.com";
+      forceSSL = true;
+      locations."/".proxyPass = "http://127.0.0.1:5232";
+    };
   };
 }
