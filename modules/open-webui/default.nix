23b80d302c411b17e59f4b51b42451426b1f7b7c
Zeal <zzzealed@proton.me>
2026-01-17 16:53:21 +0100

    update models

--- modules/open-webui/default.nix
+++ modules/open-webui/default.nix
@@ -13,8 +13,8 @@
       SCARF_NO_ANALYTICS = "True";
       OPENAI_API_BASE_URL = "https://g4f.l.zzzealed.com/v1"; # GPT4Free instance, shhh
       ENABLE_SIGNUP = "False";
-      DEFAULT_MODELS = "claude-sonnet-4.5";
-      DEFAULT_PINNED_MODELS = "claude-sonnet-4.5,deepseek-v3,gpt-5.1";
+      DEFAULT_MODELS = "claude-opus-4.5";
+      DEFAULT_PINNED_MODELS = "claude-opus-4.5,gpt-5.2-high,gemini-3-pro";
       ENABLE_PERSISTENT_CONFIG = "True"; # We need this I guess?
       ENABLE_VERSION_UPDATE_CHECK = "False";
       ENABLE_WEB_SEARCH = "True";
