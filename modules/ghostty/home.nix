02caa037f9ba354da8b0d7bd32beb94097d5d6cf
Zeal <zzzealed@proton.me>
2026-01-17 16:54:31 +0100

    favor HM package

--- modules/ghostty/home.nix
+++ modules/ghostty/home.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty;
+  };
   home.file.".config/ghostty" = {
     source = ./config;
     recursive = true;
