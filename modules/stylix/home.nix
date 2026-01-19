f6e6ad32fc894852447d48d743058d7386e532d7
Zeal <zzzealed@proton.me>
2026-01-19 04:09:56 +0100

    remove old programs

--- modules/stylix/home.nix
+++ modules/stylix/home.nix
@@ -1,9 +1,5 @@
 { ... }:
 {
-  # TODO: This is a weird place to put these but I don't know where else to put them
-  services.wpaperd.enable = true;
-  programs.swaylock.enable = true;
-
   stylix = {
     targets = {
       swaylock = {
