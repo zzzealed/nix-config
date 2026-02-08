0b7d0db27e6416aed7c1673c65e8ff5f57090663
Zeal <zzzealed@proton.me>
2026-02-08 22:13:29 +0100

    update image

--- modules/gpt4free_docker/docker-compose.nix
+++ modules/gpt4free_docker/docker-compose.nix
@@ -21,7 +21,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."gpt4free" = {
-    image = "hlohaus789/g4f:v6.9.3";
+    image = "hlohaus789/g4f:v7.0.7";
     volumes = [
       "/var/lib/gpt4free/generated_media:/app/generated_media:rw"
       "/var/lib/gpt4free/har_and_cookies:/app/har_and_cookies:rw"
