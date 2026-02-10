92b8441f79ddc182a7e5400a90ecd1cad03f5fbf
Zeal <zzzealed@proton.me>
2026-02-10 02:00:47 +0100

    add config

--- modules/waybar/home.nix
+++ modules/waybar/home.nix
@@ -2,18 +2,11 @@
 {
   programs.waybar = {
     enable = true;
-#    style = ''
-#    window#waybar {
-#      margin-top: 5px;
-#      margin-left: 5px;
-#      margin-right: 5px;
-#    }
-#    '';
   };
 
-  home.file.".config/waybar/config.jsonc" = {
-    source = ./config/config.jsonc;
-  };
+#  home.file.".config/waybar/config.jsonc" = {
+#    source = ./config/config.jsonc;
+#  };
   home.file.".config/waybar/wg-quick.sh" = {
     source = ./config/wg-quick.sh;
     executable = true;
