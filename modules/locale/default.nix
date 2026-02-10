f65451b843f847157c657d3c905e5133829fee33
Zeal <zzzealed@proton.me>
2026-02-10 01:59:53 +0100

    fix locale

--- modules/locale/default.nix
+++ modules/locale/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   time.timeZone = "Europe/Copenhagen";
-  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.defaultLocale = "da_DK.UTF-8";
     i18n.extraLocaleSettings = {
       LC_ADDRESS = "da_DK.UTF-8";
       LC_IDENTIFICATION = "da_DK.UTF-8";
@@ -12,5 +12,7 @@
       LC_PAPER = "da_DK.UTF-8";
       LC_TELEPHONE = "da_DK.UTF-8";
       LC_TIME = "da_DK.UTF-8";
+      # Keep messages english
+      LC_MESSAGES = "en_US.UTF-8";
     };
 }
