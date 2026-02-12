424a30ee7a8743bbfc7fd0bcdc759447f0e55293
Zeal <zzzealed@proton.me>
2026-02-12 21:29:32 +0100

    re-add config symlink

--- modules/waybar/home.nix
+++ modules/waybar/home.nix
@@ -4,9 +4,9 @@
     enable = true;
   };
 
-#  home.file.".config/waybar/config.jsonc" = {
-#    source = ./config/config.jsonc;
-#  };
+  home.file.".config/waybar/config.jsonc" = {
+    source = ./config/config.jsonc;
+  };
   home.file.".config/waybar/wg-quick.sh" = {
     source = ./config/wg-quick.sh;
     executable = true;
