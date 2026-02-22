d3b42d196f166295c0ff60d22d9ed8f6980e98ec
Zeal <zzzealed@proton.me>
2026-02-22 19:31:45 +0100

    remove fd skim

--- modules/cli-tools/default.nix
+++ modules/cli-tools/default.nix
@@ -2,14 +2,12 @@
 {
   imports = [
     ../dust.nix
-    ../skim.nix
     ../tree.nix
     ../tokei.nix
     ../tealdeer.nix
     ../fastfetch.nix
     ../ripgrep.nix
     ../uutils-coreutils.nix
-    ../fd.nix
     ../pastel.nix
   ];
 }
