9a5096782b93828ea2a4568bf1f96c3f02fc5366
Zeal <zzzealed@proton.me>
2026-02-22 19:33:08 +0100

    switch to flake

--- modules/glide-browser/default.nix
+++ modules/glide-browser/default.nix
@@ -1,19 +1,7 @@
-{ pkgs, ... }:
-#let
-#  glide-browser = pkgs.callPackage ./package.nix {};
-#in
+{ system, inputs, ... }:
 {
-#  environment.systemPackages = [ pkgs.ff2mpv ];
-#  programs.firefox = {
-#    enable = true;
-#    package = glide-browser;
-#    languagePacks = [ "en-US" "da" ];
-#    nativeMessagingHosts.packages = [ pkgs.ff2mpv ]; # This doesn't actually work. 
-#  };
-#  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1"; # https://wiki.nixos.org/wiki/Firefox#Wayland_support
-
-  environment.systemPackages = with pkgs; [
-    (callPackage ./package.nix {})
+  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1"; # https://wiki.nixos.org/wiki/Firefox#Wayland_support
+  environment.systemPackages = [
+    inputs.glide-browser.packages.${system}.default
   ];
-
 }
