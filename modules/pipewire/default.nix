31f53c23a7fa62b285267e37e4b0352bacc22301
Zeal <zzzealed@proton.me>
2026-02-26 21:04:18 +0100

    switch back from low latency

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -1,6 +1,5 @@
-{ inputs, ... }:
+{ ... }:
 {
-#  imports = [ inputs.nix-gaming.nixosModules.pipewireLowLatency ];
   services.pipewire = {
     enable = true;
     audio.enable = true; # Set as primary sound server
@@ -8,14 +7,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-
-#    lowLatency = {
-#      # enable this module
-#      enable = true;
-#      # defaults (no need to be set unless modified)
-#      quantum = 1024;
-#      rate = 48000;
-#    };
   };
 
   # make pipewire realtime-capable
