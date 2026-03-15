ba71486b9e545f277f213a9416576a5174f1da07
Zeal <zzzealed@proton.me>
2026-03-15 23:50:22 +0100

    switch to home-manager module

--- modules/glide-browser/default.nix
+++
@@ -1,7 +0,0 @@
-{ system, inputs, ... }:
-{
-  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1"; # https://wiki.nixos.org/wiki/Firefox#Wayland_support
-  environment.systemPackages = [
-    inputs.glide-browser.packages.${system}.default
-  ];
-}
