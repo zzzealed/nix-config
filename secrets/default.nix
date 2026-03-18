3eeb3366196accf5d5cff590f8dfc220837f5c88
Zeal <zzzealed@proton.me>
2026-03-18 14:02:15 +0100

    switch to stdenv.hostPlatform.system

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,7 +1,7 @@
-{ inputs, system, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.agenix.nixosModules.default ];
-  environment.systemPackages = [ inputs.agenix.packages.${system}.default ];
+  environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # /etc/ssh/ssh_host_ed25519_key
 }
 
