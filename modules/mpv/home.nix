c63ba5417bcb357d03e8394e975ed08d535bbea4
Zeal <zzzealed@proton.me>
2026-03-24 00:04:38 +0100

    move MPV config

--- modules/mpv/home.nix
+++ modules/mpv/home.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.file.".config/mpv" = {
     source = ./config;
     recursive = true;
+  };
+  programs.mpv = {
+    enable = true;
+    package = pkgs.unstable.mpv.override {
+      scripts = with pkgs.unstable; [
+        mpvScripts.webtorrent-mpv-hook
+        mpvScripts.sponsorblock-minimal
+        mpvScripts.mpris
+      ];
+    };
   };
 }
