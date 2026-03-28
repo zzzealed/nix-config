09240e4fa30f3d6c44b377e03ca354785143e8ad
Zeal <zzzealed@proton.me>
2026-03-28 22:25:49 +0100

    add script

--- modules/mpv/home.nix
+++ modules/mpv/home.nix
@@ -7,10 +7,11 @@
   programs.mpv = {
     enable = true;
     package = pkgs.unstable.mpv.override {
-      scripts = with pkgs.unstable; [
-        mpvScripts.webtorrent-mpv-hook
-        mpvScripts.sponsorblock-minimal
-        mpvScripts.mpris
+      scripts = with pkgs.unstable.mpvScripts; [
+        webtorrent-mpv-hook
+        sponsorblock-minimal
+        mpris
+        manga-reader
       ];
     };
   };
