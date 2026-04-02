405add24f438b799555c19355bfe9860e6f2c55b
Zeal <zzzealed@proton.me>
2026-04-02 02:16:05 +0200

    remove podman

--- modules/podman/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  virtualisation.podman = {
-    enable = true;
-    defaultNetwork.settings = {
-      #dns_enabled = lib.mkForce false; # Free port 53 for Pihole, force because of changedetection-io
-    };
-  };
-}
