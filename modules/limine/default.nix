e8da020ceec2dedf7ce660eabd2ee64c63705e53
Zeal <zzzealed@proton.me>
2026-04-02 02:15:36 +0200

    modularise dualboot

--- modules/limine/default.nix
+++ modules/limine/default.nix
@@ -3,11 +3,5 @@
   boot.loader.limine = {
     enable = true;
     package = pkgs.limine;
-    # Windows dual-boot
-    extraEntries = ''
-      /Windows 11
-          protocol: efi_chainload
-          image_path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
-    '';
   };
 }
