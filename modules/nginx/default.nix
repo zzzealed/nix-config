f437474d93bf73da9fc2ec8278f19791a7d60475
Zeal <zzzealed@proton.me>
2026-04-04 11:34:29 +0200

    removed unused secrets references

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -1,8 +1,6 @@
 { ... }:
 {
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-  age.secrets."porkbun-nginx_api_key".file = ../../secrets/porkbun-nginx_api_key.age;
-  age.secrets."porkbun-nginx_api_secret".file = ../../secrets/porkbun-nginx_api_secret.age;
   services.nginx = {
     enable = true;
     recommendedGzipSettings = true;
