8ecfec9e9926a830f1781d41411b5390adc02cd0
Zeal <zzzealed@proton.me>
2026-04-09 23:05:37 +0200

    add test

--- modules/mysql/default.nix
+++ modules/mysql/default.nix
@@ -3,5 +3,17 @@
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
+    # Create databases
+    ensureDatabases = [ "db1" ];
+  
+    # Create users and grant permissions
+    ensureUsers = [
+      {
+        name = "user1";
+        ensurePermissions = {
+          "db1.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
   };
 }
