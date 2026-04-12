2fe7b716311167fcc81c14441952a7c588261509
Zeal <zzzealed@proton.me>
2026-04-12 16:20:09 +0200

    add Wireguard IPs to allow

--- modules/samba-shares/default.nix
+++ modules/samba-shares/default.nix
@@ -12,7 +12,7 @@
         #"use sendfile" = "yes";
         #"max protocol" = "smb2";
         # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost 10.100.0.0/16"; # 10.100.0.0/16 here being my Wireguard IPs
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
