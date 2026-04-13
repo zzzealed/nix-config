4af8e518bde3fca6b585f0879b7414a3a77a8bfe
Zeal <zzzealed@proton.me>
2026-04-13 09:35:38 +0200

    switch to home-manager module

--- modules/ghostty/home.nix
+++ modules/ghostty/home.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.unstable.ghostty;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    installBatSyntax = true;
+  };
   home.file.".config/ghostty" = {
     source = ./config;
     recursive = true;
