5d3e346240f1982865fd375b7e9142f0f2e9e316
Zeal <zzzealed@proton.me>
2026-04-18 18:00:15 +0200

    add ports

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -7,6 +7,10 @@
       PermitRootLogin = "prohibit-password";
       X11Forwarding = true;
     };
+    ports = [
+      22
+      2222
+    ];
     hostKeys = [
       {
         path = "/etc/ssh/ssh_host_ed25519_key"; # /etc/ssh/ssh_host_ed25519_key
