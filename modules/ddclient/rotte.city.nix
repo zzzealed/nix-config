f0c72840c0f27cc0079b5dd413f315663de846ab
Zeal <zzzealed@proton.me>
2026-04-24 17:13:26 +0200

    remove rotte.city.nix

--- modules/ddclient/rotte.city.nix
+++
@@ -1,7 +0,0 @@
-{ config, ... }:
-{
-  age.secrets."vps-nixos_ddclient_config".file = ../../secrets/vps-nixos_ddclient_config.age;
-  services.ddclient = {
-    configFile = config.age.secrets."vps-nixos_ddclient_config".path;
-  };
-}
