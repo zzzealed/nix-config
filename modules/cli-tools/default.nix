cf2b9e41cbfa12234d38cfe2f6b9aac36e638e68
Zeal <zzzealed@proton.me>
2026-04-24 17:04:19 +0200

    adjust imports

--- modules/cli-tools/default.nix
+++ modules/cli-tools/default.nix
@@ -3,13 +3,11 @@
   imports = [
     ../dust.nix
     ../tree.nix
-    ../tokei.nix
-    ../tealdeer.nix
     ../fastfetch.nix
     ../ripgrep.nix
     ../uutils-coreutils.nix
-    ../pastel.nix
     ../gomi.nix
     ../jujutsu.nix
+    ../speedtest-cli.nix
   ];
 }
