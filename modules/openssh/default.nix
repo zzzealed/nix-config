1b4fad8fc91aba365e41c491b837ff8a87dbba6c
Zeal <zzzealed@proton.me>
2026-04-24 17:03:54 +0200

    add extraConfig

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -7,6 +7,9 @@
       PermitRootLogin = "prohibit-password";
       X11Forwarding = true;
     };
+    extraConfig = ''
+      AcceptEnv COLORTERM TERM_PROGRAM TERM_PROGRAM_VERSION
+    '';
     ports = [
       22
       2222
@@ -17,5 +20,16 @@
         type = "ed25519";
       }
     ];
+  };
+  programs.ssh = {
+    extraConfig = ''
+      Host vps.rotte.city
+        HostName vps.rotte.city
+        Port 2222
+    
+      Host *
+        SetEnv TERM=xterm-256color
+        SendEnv COLORTERM TERM_PROGRAM TERM_PROGRAM_VERSION
+    '';
   };
 }
