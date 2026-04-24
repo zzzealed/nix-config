cbd004cfb766aa6e1bfc90b4be31014614aff13a
Zeal <zzzealed@proton.me>
2026-04-24 17:10:58 +0200

    adjust config

--- modules/ddclient/default.nix
+++ modules/ddclient/default.nix
@@ -1,8 +1,6 @@
-{ config, ... }:
+{ ... }:
 {
-  age.secrets."ddclient_config".file = ../../secrets/ddclient_config.age;
   services.ddclient = {
     enable = true;
-    configFile = config.age.secrets."ddclient_config".path;
   };
 }
