355110fe6541d5dd78180e52ddb17f1027880203
Zeal <zzzealed@proton.me>
2026-04-24 17:06:48 +0200

    add peers

--- modules/wireguard/default.nix
+++ modules/wireguard/default.nix
@@ -9,16 +9,29 @@
     ips = [ "10.100.0.1/16" ];
     listenPort = 51820;
     # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-    postSetup = "${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/16 -o enu1u1u1 -j MASQUERADE";
-    postShutdown = "${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/16 -o enu1u1u1 -j MASQUERADE";
+    postSetup = "${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/16 -d 192.168.0.0/24 -o enu1u1u1 -j MASQUERADE";
+    postShutdown = "${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/16 -d 192.168.0.0/24 -o enu1u1u1 -j MASQUERADE";
 
     privateKeyFile = config.age.secrets.wireguard_private-key.path;
     peers = [
       {
-        # phone-nix
+        # 2. phone-nix
         publicKey = "bmX/vHEwIteNYly02k79UtlaEsMYz4n8aabEY6eohUA=";
         allowedIPs = [ "10.100.0.2/32" ];
       }
+      {
+        # 3. desktop-nixos
+        publicKey = "2yZDQkigD3u8+NNFbN4eP6VRB5A9qgZ/KjrF3qZCLBU=";
+        allowedIPs = [ "10.100.0.3/32" ];
+      }
+      {
+        # 4. vps-nix
+        publicKey = "oB0yJ8lGWSjpdnwRiuT7uHkN7Ri++G3K+2YmKbgzZGI=";
+        allowedIPs = [ "10.100.0.4/32" ];
+      }
+      #{
+        # 5. TODO: laptop
+      #}
     ];
   };
 
