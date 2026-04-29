d85e9c68d4cde85c8797fe1f8c96f96a7f507d34
Zeal <zzzealed@proton.me>
2026-04-29 04:07:39 +0200

    adjust config

--- modules/fish/config/config.fish
+++ modules/fish/config/config.fish
@@ -11,16 +11,16 @@ alias rsync "rsync --verbose --archive --progress --human-readable"
 
 # Rebuild alias
 function rbld-desktop
-    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname desktop-nixos --target-host mads@desktop.l.zzzealed.com --cores 10 --max-jobs 10 --ask"
+    command nh os $argv /mnt/vault/Documents/nix-config --hostname desktop-nixos --ask --max-jobs 1
 end
 function rbld-server
-    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname server-nixos --cores 10 --max-jobs 10 --ask"
+    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname server-nixos --ask"
 end
 function rbld-pi
-    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname pi-nixos --target-host mads@pi.l.zzzealed.com --cores 10 --max-jobs 10 --ask"
+    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname pi-nixos --target-host mads@pi.l.zzzealed.com --ask"
 end
 function rbld-vps
-    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname vps-nixos --target-host mads@vps.rotte.city --cores 10 --max-jobs 10 --ask"
+    command ssh -A -t mads@server.l.zzzealed.com "nh os $argv /vault/Documents/nix-config --hostname vps-nixos --target-host mads@vps.rotte.city --ask"
 end
 
 # Duration alias
