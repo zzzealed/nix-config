f4c7080aca3287481f6fcae519aa8d1f24a048ca
Zeal <zzzealed@proton.me>
2026-04-30 00:02:53 +0200

    fix system

--- overlays/nixpkgs-unstable.nix
+++ overlays/nixpkgs-unstable.nix
@@ -4,7 +4,7 @@
     (final: _prev: {
       unstable = import flakeInputs.nixpkgs-unstable {
         config.allowUnfree = true; # This is a different nixpkgs instance so we need to pass this again
-        system = final.system;
+        system = final.stdenv.hostPlatform.system;
       };
     })
   ];
