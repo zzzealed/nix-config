4e025cecf841b9970dcbe88c09a24fd7f5f1b45f
Zeal <zzzealed@proton.me>
2026-04-29 18:27:28 +0200

    add substitutes

--- flake.nix
+++ flake.nix
@@ -79,10 +79,12 @@
                       "flakes"
                     ];
                     download-buffer-size = 524288000; # https://github.com/NixOS/nix/issues/11728#issuecomment-2725297584
-                    trusted-substituters = [
+                    substituters = [
+                      "https://cache.nixos.org"
                       "https://cache.l.zzzealed.com"
                     ];
                     trusted-public-keys = [
+                      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                       "cache.l.zzzealed.com-1:d29SnNbB+hmWUlqbc6TaYaOP8fv25hlxpQRxgCqLQhE="
                     ];
                   };
