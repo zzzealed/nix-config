a450aacf725aaceb58d19653b55e7ee82516d3c8
Zeal <zzzealed@proton.me>
2026-04-30 17:13:04 +0200

    refactor aliases

--- modules/fish/config/config.fish
+++ modules/fish/config/config.fish
@@ -1,46 +1,3 @@
-# Aliases
-alias cat bat
-alias cp "uutils-cp --verbose --interactive --progress"
-alias du dust
-alias ls "ls --color=always --indicator-style=slash"
-alias mv "uutils-mv --verbose --interactive --progress"
-#alias rm "rm --verbose --interactive=once"
-alias rm "echo use Gomi!"
-alias gomi "gomi --verbose"
-alias rsync "rsync --verbose --archive --progress --human-readable"
-
-# Rebuild aliases
-function rbld
-    command nh os $argv[1] "$HOME/nix-config#$argv[2]" --ask
-end
-function rbld-desktop
-    command ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#desktop-nixos --ask --target-host mads@desktop.l.zzzealed.com"
-end
-function rbld-server
-    command ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#server-nixos --ask"
-end
-function rbld-pi
-    command ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#pi-nixos --ask --target-host mads@pi.l.zzzealed.com"
-end
-function rbld-vps
-    command ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#vps-nixos --ask --target-host mads@vps.rotte.city"
-end
-
-# Duration alias
-# TIP: use like `duration *.mkv */*.mkv | sort -V`
-function duration
-    for f in $argv
-        printf "%s - %s\n" \
-            (ffprobe -v error -show_entries format=duration -of default=nw=1:nk=1 -sexagesimal "$f") \
-            (path basename "$f")
-    end
-end
-
-# Bangers alias
-function bangers
-    command mpv /mnt/vault/Videos/Memes/bangers.m3u8 --directory-mode=ignore
-end
-
 # Interactive shell initialisation
 set fish_greeting # Disable greeting
 
@@ -50,9 +7,68 @@ set -g fish_key_bindings fish_vi_key_bindings
 # Set $EDITOR
 set -gx EDITOR hx
 
-# Send desktop-noti if command longer than 10s
-#function notify_long_tasks --on-event fish_postexec
-#    if [ "$CMD_DURATION" -gt 10000 ] # 10 seconds
-#        notify-send --app-name=Ghostty --icon=com.mitchellh.ghostty --expire-time=1000 "Command finished" $argv
-#    end
+# Custom alias function
+function als # Custom alias function
+    echo "\$ $argv"
+    command $argv
+end
+
+# Aliases
+## Rebuild
+function rbld
+    als nh os $argv[1] $HOME/nix-config#$argv[2]
+end
+function rbld-desktop
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#desktop-nixos --ask --target-host mads@desktop.l.zzzealed.com"
+end
+function rbld-server
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#server-nixos --ask"
+end
+function rbld-pi
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#pi-nixos --ask --target-host mads@pi.l.zzzealed.com"
+end
+function rbld-vps
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#vps-nixos --ask --target-host mads@vps.rotte.city"
+end
+
+## Misc.
+function cat
+    als bat
+end
+function cp
+    als uutils-cp --verbose --interactive --progress
+end
+function du
+    als dust
+end
+function ls
+    als ls --color=always --indicator-style=slash
+end
+function mv
+    als uutils-mv --verbose --interactive --progress
+end
+#function rm
+#    als rm --verbose --interactive=once
 #end
+function rm
+    als echo use Gomi!
+end
+function gomi
+    als gomi --verbose
+end
+function rsync
+    als rsync --verbose --archive --progress --human-readable
+end
+
+## Custom
+# TIP: use like `duration *.mkv */*.mkv | sort -V`
+function duration
+    for f in $argv
+        printf "%s - %s\n" \
+            (ffprobe -v error -show_entries format=duration -of default=nw=1:nk=1 -sexagesimal "$f") \
+            (path basename "$f")
+    end
+end
+function bangers
+    command mpv /mnt/vault/Videos/Memes/bangers.m3u8 --directory-mode=ignore
+end
