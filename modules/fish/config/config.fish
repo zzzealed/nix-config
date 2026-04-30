e5fe3d54594b49de574dfa610662c69fee82226c
Zeal <zzzealed@proton.me>
2026-04-30 17:24:18 +0200

    add argv

--- modules/fish/config/config.fish
+++ modules/fish/config/config.fish
@@ -16,36 +16,36 @@ end
 # Aliases
 ## Rebuild
 function rbld
-    als nh os $argv[1] $HOME/nix-config#$argv[2]
+    als nh os $argv[1] $HOME/nix-config#$argv[2] $argv[3..]
 end
 function rbld-desktop
-    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#desktop-nixos --ask --target-host mads@desktop.l.zzzealed.com"
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv[1] git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#desktop-nixos --ask --target-host mads@desktop.l.zzzealed.com" $argv[2..]
 end
 function rbld-server
-    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#server-nixos --ask"
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv[1] git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#server-nixos --ask" $argv[2..]
 end
 function rbld-pi
-    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#pi-nixos --ask --target-host mads@pi.l.zzzealed.com"
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv[1] git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#pi-nixos --ask --target-host mads@pi.l.zzzealed.com" $argv[2..]
 end
 function rbld-vps
-    als ssh -tA mads@server.l.zzzealed.com "nh os $argv git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#vps-nixos --ask --target-host mads@vps.rotte.city"
+    als ssh -tA mads@server.l.zzzealed.com "nh os $argv[1] git+ssh://mads@desktop.l.zzzealed.com/home/mads/nix-config#vps-nixos --ask --target-host mads@vps.rotte.city" $argv[2..]
 end
 
 ## Misc.
 function cat
-    als bat
+    als bat $argv
 end
 function cp
-    als uutils-cp --verbose --interactive --progress
+    als uutils-cp --verbose --interactive --progress $argv
 end
 function du
-    als dust
+    als dust $argv
 end
 function ls
-    als ls --color=always --indicator-style=slash
+    als ls --color=always --indicator-style=slash $argv
 end
 function mv
-    als uutils-mv --verbose --interactive --progress
+    als uutils-mv --verbose --interactive --progress $argv
 end
 #function rm
 #    als rm --verbose --interactive=once
@@ -54,10 +54,10 @@ function rm
     als echo use Gomi!
 end
 function gomi
-    als gomi --verbose
+    als gomi --verbose $argv
 end
 function rsync
-    als rsync --verbose --archive --progress --human-readable
+    als rsync --verbose --archive --progress --human-readable $argv
 end
 
 ## Custom
