8f58dfe41b0a433ca39669e9cf869eaa16f6982b
Zeal <zzzealed@proton.me>
2026-05-02 22:45:46 +0200

    move nix.settings outside harmonia

--- modules/harmonia/default.nix
+++ modules/harmonia/default.nix
@@ -8,8 +8,8 @@
     signKeyPaths = [ config.age.secrets."harmonia_sign-key".path ];
     # Public: cache.l.zzzealed.com-1:d29SnNbB+hmWUlqbc6TaYaOP8fv25hlxpQRxgCqLQhE=
     settings.bind = "[::]:5050";
-    nix.settings.allowed-users = [ "harmonia" ];
   };
+  nix.settings.allowed-users = [ "harmonia" ];
   services.nginx.virtualHosts."cache.l.zzzealed.com" = {
     useACMEHost = "zzzealed.com";
     forceSSL = true;
