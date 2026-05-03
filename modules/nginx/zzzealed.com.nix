aa237a3e4d9f64314ab74c973609774c2d1a21d6
Zeal <zzzealed@proton.me>
2026-05-03 22:53:35 +0200

    new revision

--- modules/nginx/zzzealed.com.nix
+++ modules/nginx/zzzealed.com.nix
@@ -2,7 +2,7 @@
 let
   website = builtins.fetchGit {
     url = "https://codeberg.org/zzzealed/zzzealed.com";
-    rev = "d9eb65a478971259d2464f65f5cd737d86bf5d66";
+    rev = "6985b7c13ff7bb9e34dcad67929733d04b0c25cd";
   };
 in
 {
@@ -22,10 +22,10 @@ in
     forceSSL = true;
     locations."/".return = "301 https://status.rotte.city/endpoints/zzzealed-com_website";
   };
-  
+
   security.acme.certs."zzzealed.com" = {
     domain = "zzzealed.com";
-    extraDomainNames = [ 
+    extraDomainNames = [
       "*.zzzealed.com" # Public, set via Porkbun DNS
       "*.l.zzzealed.com" # Local, set via Pihole
     ];
@@ -34,6 +34,6 @@ in
     credentialFiles = {
       PORKBUN_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_key".path;
       PORKBUN_SECRET_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_secret".path;
-    };    
+    };
   };
 }
