d1d62230c2d461250abd65fa9c607e7cbe3c015e
Zeal <zzzealed@proton.me>
2026-05-04 23:40:05 +0200

    add website 2

--- modules/nginx/rotte.city.nix
+++ modules/nginx/rotte.city.nix
@@ -1,4 +1,10 @@
 { config, ... }:
+let
+  website = builtins.fetchGit {
+    url = "https://codeberg.org/zzzealed/rotte.city";
+    rev = "25d4d6faefa686e51c1191bef8870d7bc5e62213";
+  };
+in
 {
   age.secrets."porkbun-nginx_api_key".file = ../../secrets/porkbun-nginx_api_key.age;
   age.secrets."porkbun-nginx_api_secret".file = ../../secrets/porkbun-nginx_api_secret.age;
@@ -7,12 +13,19 @@
     default = true;
     useACMEHost = "rotte.city";
     forceSSL = true;
-    root = "/";
-    locations."/".return = "404";
+    root = website;
+    locations."/health" = {
+      return = ''200 "ok"'';
+      extraConfig = ''
+        access_log off;
+        add_header Content-Type text/plain;
+      '';
+    };
   };
+
   security.acme.certs."rotte.city" = {
     domain = "rotte.city";
-    extraDomainNames = [ 
+    extraDomainNames = [
       "*.rotte.city" # Public, set via Porkbun DNS
     ];
     dnsProvider = "porkbun"; # Provider of DNS-challenge
@@ -20,6 +33,6 @@
     credentialFiles = {
       PORKBUN_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_key".path;
       PORKBUN_SECRET_API_KEY_FILE = config.age.secrets."porkbun-nginx_api_secret".path;
-    };    
+    };
   };
 }
