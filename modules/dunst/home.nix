b7c20398dc90c323ed391a36651fe9128d2f04e3
Zeal <zzzealed@proton.me>
2026-05-04 22:14:42 +0200

    add icon

--- modules/dunst/home.nix
+++ modules/dunst/home.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # TODO: bind `dunstctl close`
   services.dunst = {
     enable = true;
+    package = pkgs.dunst;
+    iconTheme.name = "Papirus";
+    iconTheme.package = pkgs.papirus-icon-theme;
     settings = {
       global.follow = "mouse"; # Show at focused window
     };
