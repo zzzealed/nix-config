f3d37560260faf78dfb0ae88af3aa8286b9d9939
Zeal <zzzealed@proton.me>
2026-05-12 08:43:03 +0200

    refactor shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,27 @@
-# Stolen from https://github.com/jnsgruk/nixos-config/blob/main/shell.nix
-{ pkgs ? import <nixpkgs> {} }:
-{
-  default = pkgs.mkShell {
-    name = "flake";
-    # Enable experimental features without having to specify the argument
-    NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [
-      nix
-      home-manager
-      git
-      vim
-    ];
-  };
+# A shell that bootstraps flake support
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  inherit (lock.nodes.nixpkgs.locked) rev narHash;
+  pkgs =
+    import
+      (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      })
+      {
+        config = {
+          allowUnfree = true;
+        };
+      };
+in
+pkgs.mkShell {
+  # Enable experimental features without having to specify the argument
+  NIX_CONFIG = "experimental-features = nix-command flakes";
+  packages = with pkgs; [
+    nix
+    home-manager
+    git
+    vim
+    nixos-rebuild-ng
+  ];
 }
