37568448fff6904e15a68173e70ab5765ba96b41
Zeal <zzzealed@proton.me>
2026-05-14 19:45:03 +0200

    update serverConfig

--- modules/qbittorrent/default.nix
+++ modules/qbittorrent/default.nix
@@ -5,6 +5,17 @@
     package = pkgs.qbittorrent-nox;
     torrentingPort = 41775;
     webuiPort = 1337;
+    serverConfig = {
+      Preferences = {
+        WebUI = {
+          Username = "mads";
+          Password_PBKDF2 = "@ByteArray(Lq+hF/JGD3e0oludEJ2xcQ==:n56jFApNg89rJMHKbq7c6nUkUA0C0xG8TFQ6eOV8I20TlZQP6jxc6IoBoSJcDgGZFNH/pU73Wxz0sBSJgV2Dqg==)";
+        };
+        Advanced = {
+          NetworkInterface = "wg_hbd-2";
+        };
+      };
+    };
   };
   services.nginx.virtualHosts."qbit.l.zzzealed.com" = {
     useACMEHost = "zzzealed.com";
