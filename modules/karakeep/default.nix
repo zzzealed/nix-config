56d92c6ed2a584f4a27695d70b14d7d0b527eb3a
Zeal <zzzealed@proton.me>
2026-05-20 00:38:25 +0200

    update inference

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -13,9 +13,9 @@
       DISABLE_NEW_RELEASE_CHECK = "true";
       # Inference
       OPENAI_API_KEY = "secret";
-      OPENAI_BASE_URL = "https://g4f.l.zzzealed.com/api/PollinationsAI";
-      INFERENCE_TEXT_MODEL = "claude-fast";
-      INFERENCE_IMAGE_MODEL = "claude-fast";
+      OPENAI_BASE_URL = "https://g4f.l.zzzealed.com/backend-api/v2/qwen";
+      INFERENCE_TEXT_MODEL = "qwen3.6-plus";
+      INFERENCE_IMAGE_MODEL = "qwen3.6-plus";
       INFERENCE_CONTEXT_LENGTH = "16384";
       # Crawler
       CRAWLER_FULL_PAGE_SCREENSHOT = "true";
