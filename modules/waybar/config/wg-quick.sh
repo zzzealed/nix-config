d698e3e49fd5d5ab7c949ef3883cb2a2079d3c50
Zeal <zzzealed@proton.me>
2026-05-31 00:32:13 +0200

    update unit

--- modules/waybar/config/wg-quick.sh
+++ modules/waybar/config/wg-quick.sh
@@ -3,12 +3,12 @@
 active=()
 
 while IFS= read -r iface; do
-  if systemctl is-active --quiet "wireguard-${iface}.service"; then
+  if systemctl is-active --quiet "wg-quick-${iface}.service"; then
     active+=("${iface}: active")
   fi
-done < <(systemctl list-unit-files 'wireguard-*.service' --no-legend \
+done < <(systemctl list-unit-files 'wg-quick-*.service' --no-legend \
   | awk '{print $1}' \
-  | sed 's/wireguard-\(.*\)\.service/\1/')
+  | sed 's/wg-quick-\(.*\)\.service/\1/')
 
 if (( ${#active[@]} > 0 )); then
   printf '%s\n' "${active[@]}"
