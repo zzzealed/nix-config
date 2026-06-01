9c5a8b33a051aa983bc95b94367800401edf5846
Zeal <zzzealed@proton.me>
2026-06-02 01:25:26 +0200

    update config

--- modules/harmonia/default.nix
+++ modules/harmonia/default.nix
@@ -3,11 +3,14 @@
   age.secrets."harmonia_sign-key".file = ../../secrets/harmonia_sign-key.age;
 
   services.harmonia = {
-    enable = true;
     package = pkgs.harmonia;
-    signKeyPaths = [ config.age.secrets."harmonia_sign-key".path ];
-    # Public: cache.l.zzzealed.com-1:d29SnNbB+hmWUlqbc6TaYaOP8fv25hlxpQRxgCqLQhE=
-    settings.bind = "[::]:5050";
+    daemon.enable = true;
+    cache = {
+      enable = true;
+      signKeyPaths = [ config.age.secrets."harmonia_sign-key".path ];
+      # Public: cache.l.zzzealed.com-1:d29SnNbB+hmWUlqbc6TaYaOP8fv25hlxpQRxgCqLQhE=
+      settings.bind = "[::]:5050";
+    };
   };
   nix.settings.allowed-users = [ "harmonia" ];
   services.nginx.virtualHosts."cache.l.zzzealed.com" = {
