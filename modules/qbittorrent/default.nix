d6daa921fb67d173f392589ffaf15e82b929755e
Zeal <zzzealed@proton.me>
2026-07-23 00:44:05 +0200

    update serverConfig

--- modules/qbittorrent/default.nix
+++ modules/qbittorrent/default.nix
@@ -1,22 +1,33 @@
 { config, pkgs, ... }:
 {
   imports = [ ./wireguard.nix ];
+
+  # Block qBit from using other interfaces (I have no idea what I'm doing)
+  systemd.services.qbittorrent.serviceConfig.RestrictNetworkInterfaces = "wg_proton-2 lo";
+
   services.qbittorrent = {
     enable = true;
     package = pkgs.qbittorrent-nox;
     torrentingPort = 41775;
     webuiPort = 1337;
+    extraArgs = [ "--confirm-legal-notice" ];
+    # https://github.com/qbittorrent/qBittorrent/wiki/Explanation-of-Options-in-qBittorrent
     serverConfig = {
+      LegalNotice.Accepted = true;
+      BitTorrent.Session = {
+        Interface = "wg_proton-2";
+        PerformanceWarning = true;
+      };
       Preferences = {
+        General.StatusbarExternalIPDisplayed = true;
+        Network.PortForwardingEnabled = true;
         WebUI = {
           Username = "mads";
+          # https://codeberg.org/feathecutie/qbittorrent_password
           Password_PBKDF2 = "@ByteArray(Lq+hF/JGD3e0oludEJ2xcQ==:n56jFApNg89rJMHKbq7c6nUkUA0C0xG8TFQ6eOV8I20TlZQP6jxc6IoBoSJcDgGZFNH/pU73Wxz0sBSJgV2Dqg==)";
           LocalHostAuth = false;
           AuthSubnetWhitelist = "10.100.0.0/16, 192.168.0.0/24";
           AuthSubnetWhitelistEnabled = true;
-        };
-        Advanced = {
-          NetworkInterface = "wg_proton-2";
         };
       };
     };
