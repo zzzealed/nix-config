a17d8b33956fd220bf697024414cc7f9ea83cedf
Zeal <zzzealed@proton.me>
2026-08-14 22:53:08 +0200

    remove icon definitions

--- modules/dunst/home.nix
+++ modules/dunst/home.nix
@@ -4,8 +4,6 @@
   services.dunst = {
     enable = true;
     package = pkgs.dunst;
-    iconTheme.name = "Papirus";
-    iconTheme.package = pkgs.papirus-icon-theme;
     settings = {
       global.follow = "mouse"; # Show at focused window
     };
