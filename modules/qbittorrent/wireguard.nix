65cb71816e6d717b7da94faa5083250787893f6b
Zeal <zzzealed@proton.me>
2026-08-24 17:04:19 +0200

    change listenPort

--- modules/qbittorrent/wireguard.nix
+++ modules/qbittorrent/wireguard.nix
@@ -2,7 +2,7 @@
 {
   age.secrets."proton-2_private-key-file".file = ../../secrets/proton-2_private-key-file.age;
   networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
+    allowedUDPPorts = [ 51821 ];
     checkReversePath = "loose";
   };
   networking.wireguard.interfaces.wg_proton-2 = {
@@ -10,7 +10,7 @@
       "10.2.0.2/32"
       "2a07:b944::2:2/128"
     ];
-    listenPort = 51820;
+    listenPort = 51821;
     privateKeyFile = config.age.secrets."proton-2_private-key-file".path;
     allowedIPsAsRoutes = false;
     peers = [
