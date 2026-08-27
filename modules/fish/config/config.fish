049293444e5f8d747a671759fe5986b4dccdf04c
Zeal <zzzealed@proton.me>
2026-08-27 21:20:30 +0200

    clean up config

--- modules/fish/config/config.fish
+++ modules/fish/config/config.fish
@@ -7,52 +7,6 @@ set -g fish_key_bindings fish_vi_key_bindings
 # Set $EDITOR
 set -gx EDITOR hx
 
-# zmx
-functions -c fish_prompt _original_fish_prompt 2>/dev/null
-
-function fish_prompt --description 'Write out the prompt'
-    if set -q ZMX_SESSION
-        echo -n "[$ZMX_SESSION] "
-    end
-    _original_fish_prompt
-end
-
-# Aliases
-## Rebuild
-function rbld
-    command sudo -v
-    command nh os $argv[1] $HOME/nix-config#$argv[2] $argv[3..]
-end
-function _rbld
-    set host $argv[1]
-    command ssh -tA mads@server.internal \
-        "sudo -v && \
-        rsync --archive --info=progress2 --filter=':- .gitignore' mads@server.internal:/home/mads/nix-config/ ~/nix-config/ && \
-        nh os $argv[2] ~/nix-config#$host $argv[3..]"
-end
-function rbld-server
-    _rbld server-nixos $argv
-end
-function rbld-desktop
-    _rbld desktop-nixos $argv --target-host mads@desktop.internal
-end
-function rbld-pi
-    _rbld pi-nixos $argv --target-host mads@pi.internal
-end
-function rbld-vps
-    _rbld vps-nixos $argv --target-host mads@vps.internal
-end
-function rbld-all
-    echo server
-    rbld-server $argv
-    echo desktop
-    rbld-desktop $argv
-    echo pi
-    rbld-pi $argv
-    echo vps
-    rbld-vps $argv
-end
-
 ## Misc.
 function cp
     command uutils-cp --verbose --interactive --progress $argv
