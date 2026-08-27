8a35f00c4b478244de5273ebc363164c493b118d
Zeal <zzzealed@proton.me>
2026-08-27 08:05:52 +0200

    cleanup

--- modules/ddclient/home.zzzealed.com.nix
+++
@@ -1,7 +0,0 @@
-{ config, ... }:
-{
-  age.secrets."server-nixos_ddclient_config".file = ../../secrets/server-nixos_ddclient_config.age;
-  services.ddclient = {
-    configFile = config.age.secrets."server-nixos_ddclient_config".path;
-  };
-}
