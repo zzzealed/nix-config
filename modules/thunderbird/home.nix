3257882b688e414d8688b7062b53791e4d94dcbd
Zeal <zzzealed@proton.me>
2026-09-03 18:42:13 +0200

    add config

--- modules/thunderbird/home.nix
+++ modules/thunderbird/home.nix
@@ -3,7 +3,54 @@
   programs.thunderbird = {
     enable = true;
     package = pkgs.thunderbird;
-    # TODO: profiles
-    # TODO: settings
+    languagePacks = [
+      "en-US"
+      "da"
+    ];
+    settings = {
+      "intl.regional_prefs.use_os_locales" = true;
+    };
+    profiles."mads" = {
+      isDefault = true;
+    };
+  };
+
+  accounts = {
+    calendar.accounts."Personal" = {
+      primary = true;
+      remote = {
+        type = "caldav";
+        url = "https://radicale.l.zzzealed.com/mads/ce920a41-e0f3-6f31-a4d7-b3d20ac78a67/";
+        userName = "mads";
+      };
+      thunderbird = {
+        enable = true;
+        color = "#000000";
+        profiles = [ "mads" ];
+      };
+    };
+    email.accounts."Personal" = {
+      primary = true;
+      address = "mads@mhjensen.com";
+      realName = "Mads H. Jensen";
+      userName = "mads@mhjensen.com";
+      smtp = {
+        host = "smtp.porkbun.com";
+        port = 587;
+        tls = {
+          enable = true;
+          useStartTls = true;
+        };
+      };
+      imap = {
+        host = "imap.porkbun.com";
+        port = 993;
+        tls.enable = true;
+      };
+      thunderbird = {
+        enable = true;
+        profiles = [ "mads" ];
+      };
+    };
   };
 }
