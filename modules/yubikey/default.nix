8aa6739c85951e50dc07d1fa073943203d1154e9
Zeal <zzzealed@proton.me>
2026-09-03 18:44:34 +0200

    update RUN

--- modules/yubikey/default.nix
+++ modules/yubikey/default.nix
@@ -8,14 +8,13 @@
     sudo.u2f.enable = true;
   };
 
-  # Lock on disconnect
-  # this barely works and also sucks ass
+  # https://wiki.nixos.org/wiki/Yubikey#Locking_the_screen_when_a_Yubikey_is_unplugged
   services.udev.extraRules = ''
     ACTION=="remove",\
      ENV{ID_BUS}=="usb",\
      ENV{ID_MODEL_ID}=="0407",\
      ENV{ID_VENDOR_ID}=="1050",\
      ENV{ID_VENDOR}=="Yubico",\
-     RUN+="${pkgs.util-linux}/bin/runuser -u mads -- ${pkgs.coreutils}/bin/env XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1 ${pkgs.swaylock}/bin/swaylock"
+     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
   '';
 }
